0202626ff9303812e2429f6b5d8ed631e565ffc5
Veronchi <zhigunova1995@gmail.com>
2022-01-15 16:02:07 +0300

    implement layout for navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,30 @@ import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img className='nav-logo' src={logo} alt='nav-logo'/>
+          <button className='btn toggle-btn'>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn'>products</button>
+          </li>
+          <li>
+            <button className='link-btn'>developers</button>
+          </li>
+          <li>
+            <button className='link-btn'>company</button>
+          </li>
+        </ul>
+        <button className='btn signin-btn'>sign in</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
