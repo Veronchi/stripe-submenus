3bd927224400385627f43d4648da3a3dc85ca217
Veronchi <zhigunova1995@gmail.com>
2022-01-17 16:06:48 +0300

    implement adding coords into the style of aside el

--- src/Submenu.js
+++ src/Submenu.js
@@ -5,18 +5,27 @@ const Submenu = () => {
   const { subLinksArr, coordinatesValue, btnValue } = useGlobalContext();
 
   const [links, setLinks] = useState([]);
+  const submenuRef = useRef();
 
+  console.log(submenuRef)
   useEffect(() => {
     if (btnValue !== '') {
       let { links } = subLinksArr.find((item) => item.page === btnValue)
 
       setLinks(links);
     }
-
   }, [btnValue, subLinksArr])
 
+  useEffect(() => {
+    const {left, top} = coordinatesValue;
+
+    submenuRef.current.style.left = `${left + 80}px`
+    submenuRef.current.style.top = `${top + 46}px`
+
+  }, [coordinatesValue])
+
   return (
-    <aside className='submenu'>
+    <aside className='submenu' ref={submenuRef}>
       <section>
         <h4>{btnValue}</h4>
         <div className='submenu-center'>
