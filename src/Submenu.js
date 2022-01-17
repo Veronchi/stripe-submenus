b013ad2528a7083071816f92fbdc75fc3617dcb2
Veronchi <zhigunova1995@gmail.com>
2022-01-17 15:47:06 +0300

    implement getting current values of links

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,14 +2,34 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  const{subLinksArr} = useGlobalContext();
+  const { subLinksArr, coordinatesValue, btnValue } = useGlobalContext();
+
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    if (btnValue !== '') {
+      let { links } = subLinksArr.find((item) => item.page === btnValue)
+
+      setLinks(links);
+    }
+
+  }, [btnValue, subLinksArr])
 
   return (
     <aside className='submenu'>
-       <section>
-         <h4>1</h4>
-         <div className='submenu-center'></div>
-       </section>
+      <section>
+        <h4>{btnValue}</h4>
+        <div className='submenu-center'>
+          {links.map(({ label, icon, url }, id) => {
+            return (
+              <a key={`id${id}`} href={url}>
+                {icon}
+                {label}
+              </a>
+            )
+          })}
+        </div>
+      </section>
     </aside>
   )
 }
--- src/context.js
+++ src/context.js
@@ -4,12 +4,18 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 function AppProvider({ children }) {
-  const [subLinksArr, setSubLinksArr] = useState([]);
+  const [subLinksArr, setSubLinksArr] = useState(sublinks);
+  const [coordinatesValue, setCoordinatesValue] = useState({});
+  const [btnValue, setBtnValue] = useState('');
 
   return (
     <AppContext.Provider
       value={{
         subLinksArr,
+        coordinatesValue,
+        btnValue,
+        setCoordinatesValue,
+        setBtnValue
       }}
     >
       {children}
