a5d611f6a0a258b2136589437f1f2ced13c9c9db
Veronchi <zhigunova1995@gmail.com>
2022-01-20 14:34:06 +0300

    refactor the submenu component

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,23 +2,32 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  const { currentPage: {page, links}, coordinatesValue, isSubmenuOpen } = useGlobalContext();
+  const { currentPage: { page, links }, coordinatesValue, isSubmenuOpen } = useGlobalContext();
 
+  const [columns, setColumns] = useState('')
   const submenuRef = useRef();
 
   useEffect(() => {
-    const {roundedLeft, top} = coordinatesValue;
-
-    submenuRef.current.style.left = `${roundedLeft + 80}px`
-    submenuRef.current.style.top = `${top + 46}px`
-
-  }, [coordinatesValue])
+    setColumns('col-2')
+    const { roundedLeft, top } = coordinatesValue;
+    submenuRef.current.style.left = `${roundedLeft + 80}px`;
+    submenuRef.current.style.top = `${top + 46}px`;
+    if (links.length === 3) {
+      setColumns('col-3')
+    }
+    if (links.length > 3) {
+      setColumns('col-4')
+    }
+  }, [coordinatesValue, links])
 
   return (
-    <aside className={isSubmenuOpen ? 'submenu show' : 'submenu'} ref={submenuRef}>
+    <aside
+      className={isSubmenuOpen ? 'submenu show' : 'submenu'}
+      ref={submenuRef}
+    >
       <section>
         <h4>{page}</h4>
-        <div className={isSubmenuOpen ? `submenu-center col-${links.length}` : 'submenu-center'}>
+        <div className={`submenu-center ${columns}`}>
           {links.map(({ label, icon, url }, id) => {
             return (
               <a key={`id${id}`} href={url}>
