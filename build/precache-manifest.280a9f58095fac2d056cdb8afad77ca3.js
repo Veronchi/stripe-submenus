063c2f10f4a451a0441a7d09884d20ac78b9bff4
Veronchi <zhigunova1995@gmail.com>
2022-02-20 18:01:20 +0300

    add build folder

---
+++ build/precache-manifest.280a9f58095fac2d056cdb8afad77ca3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b636eb684e1578c8c0454aa17053e57",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35bb6f826489e49cc388",
+    "url": "./static/css/main.9d3ce466.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "./static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35bb6f826489e49cc388",
+    "url": "./static/js/main.cc17b498.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "./static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "./static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "./static/media/phone.58d7e3d6.svg"
+  }
+]);
